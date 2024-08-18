405930aabae611229e12851321ef6b80df3c8dc3
Сергей Тынский <tynskysergei@yandex.ru>
2024-08-18 23:41:49 +0300

    cod

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,29 +1,45 @@
-export {handleFormSubmit, handleAddCard};
-import {createCard, closeModal} from './card';
-
-const gallery = document.querySelector('.places__list')
-const nameInput = document.querySelector('.popup__input_type_name');
-const jobInput = document.querySelector('.popup__input_type_description');
-const nameTitle = document.querySelector('.profile__title');
-const jobTitle = document.querySelector('.profile__description');
-const nameAdd = document.querySelector('.popup__input_type_card-name');
-const linkAdd = document.querySelector('.popup__input_type_url');
-
-const handleFormSubmit = (evt) => {
-  evt.preventDefault();
-  nameTitle.textContent = nameInput.value
-  jobTitle.textContent = jobInput.value
-  const popup = evt.target.closest('.popup')
-  closeModal(popup)
-}
+export {openPopup, openImgPopup, handleEscKeyUp, closeModal, initPopupListeners}
 
 
-const handleAddCard = (evt) => {
-  evt.preventDefault();
-  const card = createCard(nameAdd.value, linkAdd.value);
-  gallery.append(card);
-  const popup = evt.target.closest('.popup')
-  closeModal(popup)
-  nameAdd.value = ""
-  linkAdd.value = ""
+const imgPopup = document.querySelector('.popup_type_image');
+const popupCaption = document.querySelector('.popup__caption')
+const photoPopup = document.querySelector('.popup__image');
+
+function openPopup(popup) {
+  popup.classList.add('popup_is-animated');
+  popup.classList.add('popup_is-opened');
+  document.addEventListener('keydown', handleEscKeyUp)
+};
+
+function openImgPopup(link, name) {
+  openPopup(imgPopup);
+  photoPopup.src = link
+  photoPopup.alt = name
+  popupCaption.textContent = name
+};
+
+const handleEscKeyUp = (evt) => {
+  if (evt.key === "Escape") {
+    const popupOpen = document.querySelector(".popup_is-opened");
+    closeModal(popupOpen);
+  }
+};
+
+const closeModal = (modal) => {
+  modal.classList.remove('popup_is-opened')
+  document.removeEventListener('keydown', handleEscKeyUp);
+};
+
+
+const initPopupListeners = (popup) => {
+  const cross = popup.querySelector('.popup__close')
+  cross.addEventListener("click", () => {
+    closeModal(popup)
+  });
+  
+  popup.addEventListener('mousedown' , (evt) => {
+    if (evt.target === popup) {
+      closeModal(popup)
+    }
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import '../pages/index.css';
-import {openPopup, listern} from './components/card';
-import {handleFormSubmit, handleAddCard} from './components/modal';
+import {openPopup, openImgPopup, handleEscKeyUp, closeModal, initPopupListeners} from './components/modal'
+import {initialCards} from './components/cards'
+import {createCard, deleteCard, like} from './components/card'
 
 
 const addCardBtn = document.querySelector('.profile__add-button')
@@ -12,7 +13,14 @@ const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_description');
 const nameTitle = document.querySelector('.profile__title');
 const jobTitle = document.querySelector('.profile__description');
+const gallery = document.querySelector('.places__list')
+const nameAdd = document.querySelector('.popup__input_type_card-name');
+const linkAdd = document.querySelector('.popup__input_type_url');
 
+
+initialCards.forEach((item) => {
+  gallery.append(createCard(item.name, item.link, deleteCard, like, openImgPopup));
+});
 
 addCardBtn.addEventListener('click', () => {openPopup(addPopup)});
 editBtn.addEventListener('click', () => {
@@ -21,11 +29,31 @@ editBtn.addEventListener('click', () => {
   jobInput.value = jobTitle.textContent
 });
 
-// удаление popuplistern(editPopup)
-listern(editPopup)
-listern(addPopup)
-listern(imgPopup)
+initPopupListeners(editPopup)
+initPopupListeners(addPopup)
+initPopupListeners(imgPopup)
 
-// Форма редактирования
-editPopup.addEventListener('submit', handleFormSubmit);
+editPopup.addEventListener('submit', handleEditProfile);
+addPopup.addEventListener('submit', handleAddCard);
+
+const handleEditProfile = (evt) => {
+  evt.preventDefault();
+  nameTitle.textContent = nameInput.value
+  jobTitle.textContent = jobInput.value
+  const popup = evt.target.closest('.popup')
+  closeModal(popup)
+}
+
+
+const handleAddCard = (evt) => {
+  evt.preventDefault();
+  const card = createCard(nameAdd.value, linkAdd.value, deleteCard, like, openImgPopup);
+  gallery.prepend(card);
+  const popup = evt.target.closest('.popup')
+  closeModal(popup)
+  nameAdd.value = ""
+  linkAdd.value = ""
+}
+
+editPopup.addEventListener('submit', handleEditProfile);
 addPopup.addEventListener('submit', handleAddCard);
